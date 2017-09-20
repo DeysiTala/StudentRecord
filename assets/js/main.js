ab196a8a81e254dea1d6215d8b7eef6723ca903e
Deysi <sude.90@gmail.com>
2017-09-20 16:32:33 -0500

    falta el otro boton funcionalidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,31 @@
-+function () {
+function records() {
     // Elementos
     var botonAgregar = document.getElementById("agregar");
     var botonMostrar = document.getElementById("mostrar");
-    var botonBuscar = document.getElementById("buscar");
-    var botonTopTecnico = document.getElementById("top-tecnico");
-    var botonTopHSE = document.getElementById("top-hse");
-    var resultado = document.getElementById("contenedor-estudiantes");
-    
+    var botonUp = document.getElementById("up")
+    var resultado = document.getElementById("container-students");
+
     // Evento Click - Agregar
-    var eventoAgregar = function (e) {
+    var eventoAgregar = function(e) {
         e.preventDefault();
         var estudiante = agregarEstudiante();
         resultado.innerHTML = mostrar(estudiante);
     };
-    
-    var eventoMostrar = function (e) {
+
+    var eventoMostrar = function(e) {
         e.preventDefault();
         var estudiantes = obtenerListaEstudiantes();
         resultado.innerHTML = mostrarLista(estudiantes);
     };
-        
-    var eventoBuscar = function (e) {
-        e.preventDefault();
-        var estudiantes = obtenerListaEstudiantes();
-        var nombreEstudiante = prompt("¿Qué nombre desea buscar?");
-        var estudianteBuscado = buscar(nombreEstudiante, estudiantes);
-        resultado.innerHTML = mostrarLista(estudianteBuscado);
-    };
-    
-    var eventoTopTecnico = function (e) {
-        e.preventDefault();
-        var estudiantes = obtenerListaEstudiantes();
-        var estudiantesTopTecnico = topTecnico(estudiantes);
-        resultado.innerHTML = mostrarLista(estudiantesTopTecnico);
-    };
-    
-    var eventoTopHSE = function (e) {
-        e.preventDefault();
-        var estudiantes = obtenerListaEstudiantes();
-        var estudiantesTopHSE = topHSE(estudiantes);
-        resultado.innerHTML = mostrarLista(estudiantesTopHSE);
-    };
-    
+  
     // Manejadores de eventos
     botonAgregar.addEventListener("click", eventoAgregar);
     botonMostrar.addEventListener("click", eventoMostrar);
-   
-}();
+
+
+
+}
+records();
 
 var estudiantes = [];
 
@@ -60,32 +39,25 @@ function agregarEstudiante() {
     // Hint: El estudiante debe ser agregado a la lista de estudiantes
     // TO DO: Retornar el estudiante recientemente creado
     var nombre = prompt("Nombre de la estudiante:");
-      var porcentajeTecnico = prompt("Puntos técnicos:");
-      var porcentajeSocio = prompt("Puntos Socio-Emocionales:");
-      var estudiante =
-      {
-        name : nombre,
-        tech :porcentajeTecnico,
-        social : porcentajeSocio
-      };
-      estudiantes.push(estudiante);
-      return estudiante;
+    var porcentajeTecnico = prompt("Puntos técnicos:");
+    var porcentajeSocio = prompt("Puntos Socio-Emocionales:");
+    var estudiante = {
+        name: nombre,
+        tech: porcentajeTecnico,
+        social: porcentajeSocio
+    };
+    estudiantes.push(estudiante);
+    return estudiante;
 }
 
 function mostrar(estudiante) {
     // TO DO: Completar el template para que muestre las propiedades correctas del estudiante
     var resultado = "";
-    resultado += "<div class='row'>";
-    resultado += "<div class='col m12'>";
-    resultado += "<div class='card blue-grey darken-1'>";
-    resultado += "<div class='card-content white-text'>";
     resultado += "<p><strong>Nombre:</strong> " + estudiante.name + "</p>";
-    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.tech + "</p>";
+    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.tech + "% </p>";
     resultado += "<p><strong>Puntos HSE:</strong> " + estudiante.social + "</p>";
-    resultado += "</div>";
-    resultado += "</div>";
-    resultado += "</div>";
-    resultado += "</div>";
+    resultado += "<p><strong>Estado:</strong> Activo";
+
     return resultado;
 }
 
@@ -93,12 +65,19 @@ function mostrarLista(estudiantes) {
     // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
     // Retornar el template de todos los estudiantes
     var list = "";
-    for(var i in estudiantes)
-    {
-      lista += mostrar(estudiantes[i]);
+    for (var i in estudiantes) {
+        list += mostrar(estudiantes[i]);
     }
     return list;
 }
 
 
 
+
+function checkAdult(porcentajeTecnico,porcentajeSocio) {
+    return (porcentajeSocio<=70 & porcentajeTecnico<=70);
+}
+
+function myFunction() {
+    document.getElementById("container-students").innerHTML = estudiantes.filter(checkAdult);
+}
